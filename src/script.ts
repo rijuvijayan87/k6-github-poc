a43631b8ccb980791e362a810f568ef1fe969d32
Riju Vijayan <rijuvijayan@Rijus-MacBook-Pro.local>
2022-05-26 17:19:03 +1200

    test

---
+++ src/script.ts
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { SharedArray } from 'k6/data';
+
+export const options = {
+  vus: 1,
+  duration: '10s',
+};
+
+const data = new SharedArray('some name', function () {
+  const f = JSON.parse(open('../accounts.json'));
+  return f; // f must be an array
+});
+
+export default function () {
+  console.log(`Vuser : ${__VU}. Data : ${data[__VU - 1]}`);
+  http.get('http://test.k6.io');
+  sleep(1);
+}
